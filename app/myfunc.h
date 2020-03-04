b3dae64b40f847c7858b52f54c76bbcf6489058a
Evgeniya Balyuk <balyuk@cs.petrsu.ru>
2020-03-04 11:01:57 +0300

    ok

--- app/myfunc.h
+++ app/myfunc.h
@@ -5,4 +5,6 @@ int myfunc(int b);
 
 int fibonachi(int num);
 
+int roots(int a, int b, int c)
+
 #endif // MYFUNC_H
---
+++ app/roots.c
@@ -0,0 +1,40 @@
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include"myfunc.h"
+
+int roots()
+{
+    float a, b, c;
+    float d, x1, x2;
+
+    printf("Enter a: ");
+    scanf("%f", &a);
+
+    printf("Enter b: ");
+    scanf("%f", &b);
+
+    printf("Enter c: ");
+    scanf("%f", &c);
+
+    if (a == 0){
+        x1 = (-c) / b;
+        printf ("%f", x1);
+    } else {
+        d = (b*b) - (4 * a * c);
+
+        if (d < 0)
+            printf ("Roots doesn't exist");
+        else
+            if (d == 0) {
+                x1 = (-b) / (2 * a);
+                printf ("%f", x1);
+            } else {
+                d = sqrt(d);
+                x1 = ((-b) + d) / (2 * a);
+                x2 = ((-b) - d) / (2 * a);
+                printf ("%f %f", x1, x2);
+            }
+        }
+    return 0;
+}
